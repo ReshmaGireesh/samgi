88291f187e61f0c65ef182207d79f98b1ce11947
ReshmaGireesh <sheejareshma2@gmail.com>
2021-04-21 15:20:31 +0530

    changed percentage

--- samgit/he/views.py
+++ samgit/he/views.py
@@ -4,11 +4,11 @@ from pip._vendor import requests
 
 # Create your views here.
 def home(request):
-    newurl = "https://api.thingspeak.com/channels/1360342/fields/1.json?api_key=N8YVC3T7J9UJFPNV&results=1"
+    newurl = " https://api.thingspeak.com/channels/1360342/feeds.json?api_key=N8YVC3T7J9UJFPNV&results=1"
     print(newurl)
     get_data = requests.get(newurl).json()
     k = (get_data['feeds'])
     print(k)
-    g = (k[0]['field1'])
+    g = (k[0]['field2'])
     n = k[0]['created_at']
     return render(request, 'index.html', {'field': g, 'created': n})
